333689d326bb00f9e43b616c4aa4167a93bc02de
Sondos Mohamed <39069270+So47@users.noreply.github.com>
2024-03-24 14:57:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        target_index = bisect_right(letters,target) ## if the target already exists bisect_right ensures that the new value will be insterted to the most right
-        return letters[target_index] if target_index<len(letters)  else letters[0]
-    #    low = 0
-    #    high = len(letters) - 1
+        # target_index = bisect_right(letters,target) ## if the target already exists bisect_right ensures that the new value will be insterted to the most right
+        # return letters[target_index] if target_index<len(letters)  else letters[0]
+       low = 0
+       n = len(letters)
+       high = n - 1
 
-    #    while low <= high:
-    #         mid = (low+high)>>1
-    #         guess = letters[mid]
-    #         if guess == target:
-    #             return letters[mid + 1]
-    #         elif guess > target:
-    #             high = mid -1
-    #         else:
-    #             low = mid + 1
-    #    return letters[0]         
+       while low <= high:
+            mid = (low+high)>>1
+            guess = letters[mid]
+            if guess > target:
+                high = mid -1
+            else:
+                low = mid + 1
+       return letters[low] if low < n else letters[0]         
 
                     
